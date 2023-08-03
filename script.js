227275e10f5d889f35eee8a2224a3dc36a5ca576
xebec19 <rohant946@gmail.com>
2023-08-03 20:00:30 +0530

    fix: book a call

--- script.js
+++ script.js
@@ -12,20 +12,14 @@ $(document).ready(function () {
 
   // open dayschedule pop up when user clicks on create-meeting
   $("#create-meeting").on("click", function () {
-    // daySchedule.initPopupWidget({
-    //   url: "https://rohankumarthakur.dayschedule.com",
-    //   type: "popup",
-    //   color: {
-    //     primary: "#0f0980",
-    //     secondary: "#afeefe",
-    //   },
-    // });
-    let customEvent = {
-      id: 123,
-      categoryId: 123,
-      name: "Rohan",
-    };
-    window.clarity("set", "custom-param", customEvent);
+    daySchedule.initPopupWidget({
+      url: "https://rohankumarthakur.dayschedule.com",
+      type: "popup",
+      color: {
+        primary: "#0f0980",
+        secondary: "#afeefe",
+      },
+    });
   });
 });
 
